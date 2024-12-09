79ed806fcb823f03bcf027f145dbc63ead4da1b6
LenaPancher <lenapancher@gmail.com>
2024-12-09 15:09:53 +0100

    fix hardhat.config.ts and vite-env.d.ts

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,24 +1,17 @@
+import deploy from "./scripts/deploy";
 require("@nomiclabs/hardhat-waffle");
 import "@nomiclabs/hardhat-ethers";
 import { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
+
+deploy.setDescription("Deploy the Proposal contract");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   typechain: {
     outDir: "typechain-types",
     target: "ethers-v5",
-  },
-  networks: {
-    hardhat: {
-      chainId: 1337,
-    },
-    amoy: {
-      chainId: 80002,
-      url: "https://rpc-amoy.polygon.technology",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
-    },
-  },
+  }
 };
 
 export default config;
